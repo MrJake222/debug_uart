f879f801583de72e0c639e4852106bc1caa86cf4
MrJake222 <norbertzpilicy@gmail.com>
2024-06-02 12:53:17 +0200

    new upload script

--- upload.sh
+++ upload.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-#./debug.py -p /dev/ttyACM1 -b 115200 run -n
-./debug.py -p /dev/ttyACM1 -b 115200 write --ihex $1 -v
-./debug.py -p /dev/ttyACM1 -b 115200 reset
-#./debug.py -p /dev/ttyACM1 -b 115200 run -f
+OPTS="--d32 -p /dev/ttyACM1 -b 1000000"
+
+#./debug.py $OPTS run -n
+
+./debug.py $OPTS write --ihex $1 -v
+./debug.py $OPTS reset
+
+#./debug.py $OPTS run -f
