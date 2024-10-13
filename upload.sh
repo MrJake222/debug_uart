7f222caa5318958c067831cf8e98135a6a7ee620
MrJake222 <norbertzpilicy@gmail.com>
2024-10-13 15:58:19 +0200

    Symlink to upload.sh supported

--- upload.sh
+++ upload.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+O=$PWD
+P=`readlink "$0"`
+P=`dirname "$P"`
+cd "$P"
+
+if [[ $# != 1 ]]; then
+	echo "Usage: upload.sh [hex name]"
+	exit 1
+fi
+
 OPTS="--d32 -p /dev/ttyACM1 -b 1000000"
 
 #./debug.py $OPTS run -n
